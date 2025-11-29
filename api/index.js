e480e3f6e40b1f4fe8ef06b3b5bb2c767ce865d9
Mehmet ASLANTAŞ <92733161+aslantasmehmet@users.noreply.github.com>
2025-11-29 17:41:17 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,7 @@
-const app = require('../server');
-module.exports = (req, res) => app(req, res);
+// api/index.js dosyasının TAMAMI:
+
+const app = require('../server'); // Dikkat: '../server' kullanıyoruz çünkü server.js bir üstte
+
+module.exports = (req, res) => {
+  return app(req, res);
+};
